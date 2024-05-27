6b9ea2b413074db3d35fcf046efc748215f6beb9
gabymello <mellogaby689@gmail.com>
2024-05-27 17:20:29 -0300

    jogo de adivinhação

--- jogodaadivinhacao/src/jogodaadivinhacao/Jogodaadivinhacao.java
+++ jogodaadivinhacao/src/jogodaadivinhacao/Jogodaadivinhacao.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+ 
 package jogodaadivinhacao;
+
+import java.util.Random;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,7 +16,34 @@ public class Jogodaadivinhacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        int numeroAleatorio = new Random().nextInt(10) + 1;
+        int tentativas = 0;
+        int contador = 0;
+
+        String nome = JOptionPane.showInputDialog(null, "Olá,qual o seu nome?");
+
+        System.out.println("Você terá 5 chances para adivinhar o número.");
+        int numero = 0;
+        while (numero != numeroAleatorio) {
+            contador ++;
+            System.out.println("Adivinhe o número?");
+
+            numero = sc.nextInt();
+
+            if (numero == numeroAleatorio) {
+                System.out.println("Você acetou!!" + "Em " + contador + " vezes");
+
+            } else if (numero > numeroAleatorio) {
+                System.out.println("O número que foi digitado é maior que o número sorteado.");
+            } else {
+               
+                System.out.println("O número que foi digitado é menor que o número sorteado.");
+
+            }
+        }
+            
     }
     
 }
